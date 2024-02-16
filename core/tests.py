93d414f0484c32200c612dbd866aabc427c48e25
Em Linso <em.r.linso@gmail.com>
2024-02-16 13:37:58 -0700

    Add login form

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,6 @@
-import unittest
 from django.test import TestCase
-from django.urls import reverse, resolve
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class ViewTestMixin:
@@ -35,3 +33,29 @@ class LoginViewTest(ViewTestMixin, TestCase):
     url = "/accounts/login/"
     url_name = "core:login"
     template_name = "registration/login.html"
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.username = "testuser"
+        cls.password = "12345"
+
+        cls.user = get_user_model().objects.create_user(
+            username=cls.username, password=cls.password
+        )
+
+    def test_redirect_when_login_succeeds(self):
+        response = self.client.post(
+            reverse(self.url_name),
+            {"username": self.username, "password": self.password},
+        )
+        self.assertRedirects(response, reverse("core:homepage"))
+
+    def test_show_error_when_login_fails(self):
+        response = self.client.post(
+            reverse(self.url_name),
+            {"username": "wrong", "password": "wrong"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            "Please enter a correct username and password." in response.content.decode()
+        )
