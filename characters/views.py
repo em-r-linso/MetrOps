18fa7c2fda018ee09b9abed5b034eed291d4ac90
Em Linso <em.r.linso@gmail.com>
2024-02-18 01:34:06 -0700

    Add list of owned characters to characters:index

--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,17 @@
+from typing import Any
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Character
 
 
 class Index(LoginRequiredMixin, TemplateView):
     template_name = "characters/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # get just the characters owned by the current user
+        context["characters"] = Character.objects.filter(owner=self.request.user)
+
+        return context
