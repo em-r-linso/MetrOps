76df0458a45d74556a03ca05912bf0031975810f
Em Linso <em.r.linso@gmail.com>
2024-02-18 13:02:35 -0700

    Add lifepath question and answer models

--- characters/admin.py
+++ characters/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import Character
+from .models import Character, LifepathQuestion, LifepathAnswer
 
-admin.site.register(Character)
+
+class LifepathAnswerInline(admin.TabularInline):
+    model = LifepathAnswer
+    extra = 3
+    fk_name = "question"
+    fields = ["content", "followup_question_override"]
+
+
+class LifepathQuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Content", {"fields": ["content"]}),
+        ("Followup Question", {"fields": ["followup_question"]}),
+    ]
+    inlines = [LifepathAnswerInline]
+
+
+class CharacterLifepathAnswerInline(admin.TabularInline):
+    model = Character.lifepath_answers.through
+    extra = 3
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    inlines = [CharacterLifepathAnswerInline]
+
+
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(LifepathQuestion, LifepathQuestionAdmin)
--- characters/models.py
+++ characters/models.py
@@ -2,6 +2,37 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+class LifepathQuestion(models.Model):
+    content = models.CharField(max_length=200)
+    followup_question = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="previous_questions",
+        # if no question is next, this is treated as the final question in the lifepath
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.content
+
+
+class LifepathAnswer(models.Model):
+    content = models.CharField(max_length=200)
+    question = models.ForeignKey(
+        LifepathQuestion, on_delete=models.CASCADE, related_name="lifepath_answers"
+    )
+    followup_question_override = models.ForeignKey(
+        LifepathQuestion,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.content
+
+
 class Character(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,5 +42,17 @@ class Character(models.Model):
         get_user_model(), on_delete=models.CASCADE, related_name="characters"
     )
 
+    lifepath_answers = models.ManyToManyField(
+        LifepathAnswer, through="CharacterLifepathAnswer", related_name="characters"
+    )
+
     def __str__(self):
         return self.name
+
+
+class CharacterLifepathAnswer(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    answer = models.ForeignKey(LifepathAnswer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.character.name} - {self.answer.content}"
